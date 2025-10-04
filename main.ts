e81e6545bd2d2ea2ea3ccaa3e6a7b1ff1a906cc2
m-13457847-ship-it <m-13457847@moe-dl.edu.my>
2025-10-04 12:48:39 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P0, 1)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 0)
+basic.pause(3000)
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P1, 1)
+pins.digitalWritePin(DigitalPin.P2, 0)
+basic.pause(1000)
+basic.forever(function () {
+	
+})
